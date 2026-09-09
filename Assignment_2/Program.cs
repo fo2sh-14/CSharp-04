c04d761381c14132021a79d3a893aadf8a6f722f
youssef fouad <yfo2sh@outlook.com>
2026-09-09 05:58:43 +0300

    2st answer

--- Assignment_2/Program.cs
+++ Assignment_2/Program.cs
@@ -6,12 +6,19 @@ namespace Assignment_2
     {
         static void Main(string[] args)
         {
-            #region 1st answer
-            string title = "clean code";
-            string upperTitle = title.ToUpper();
+            //#region 1st answerr
+            //string title = "clean code";
+            //string upperTitle = title.ToUpper();
 
-            Console.WriteLine(title);
-            Console.WriteLine(upperTitle); 
+            //Console.WriteLine(title);
+            //Console.WriteLine(upperTitle);
+            //#endregion
+
+            #region 2st answer 
+            string title1 = "clean code";
+            string title2 = "clean code";
+
+            Console.WriteLine(ReferenceEquals(title1, title2));
             #endregion
 
         }
