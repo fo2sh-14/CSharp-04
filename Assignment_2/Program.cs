fcaf06e064b23c3452dca628c68368eee10752bb
youssef fouad <yfo2sh@outlook.com>
2026-09-09 05:51:53 +0300

    Add project files.

---
+++ Assignment_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace Assignment_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1st answer
+            string title = "clean code";
+            string upperTitle = title.ToUpper();
+
+            Console.WriteLine(title);
+            Console.WriteLine(upperTitle); 
+            #endregion
+
+        }
+    }
+}
